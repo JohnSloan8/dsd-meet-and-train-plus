9043c5a2c8bc0d2e99f074a3d62abbdae77f9b7a
JohnSloan8 <johnsloan1984@gmail.com>
2022-12-09 19:20:09 +0000

    done

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { withErrorHandler } from '@/error-handling';
 import AppErrorBoundaryFallback from '@/error-handling/fallbacks/App';
 import Pages from '@/routes/Pages';
 import Header from '@/sections/Header';
-import HotKeys from '@/sections/HotKeys';
 import Notifications from '@/sections/Notifications';
 import SW from '@/sections/SW';
 import Sidebar from '@/sections/Sidebar';
@@ -33,7 +32,7 @@ function App() {
     <Fragment>
       <CssBaseline />
       <Notifications />
-      <HotKeys />
+
       <SW />
       <BrowserRouter>
         <Header />
